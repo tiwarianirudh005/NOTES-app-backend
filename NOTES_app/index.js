f9b5e79400c08ca629e2c7405fc6d4d83693aa54
Anirudh Tiwari <aniirudhtiwari@gmail.com>
2026-09-09 01:25:55 +0530

    Clear database connection string in index.js
    Removed sensitive database connection string.

--- NOTES_app/index.js
+++ NOTES_app/index.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const app = express();
 
 const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_gA8HYmJtpeN6@ep-sweet-glade-az76dyim-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+  connectionString: '',
   ssl: { rejectUnauthorized: false }
 });
 
